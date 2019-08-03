def3ba2c17804d0209ea9b9a0cc4bc39dd76709d
Matican Matican <maticangroup@gmail.com>
2019-08-03 10:02:37 +0430

    hossein, fixing kernel issue

--- src/Kernel.php
+++ src/Kernel.php
@@ -18,9 +18,8 @@ class Kernel extends BaseKernel
 
     public function registerBundles(): iterable
     {
-        if (false) {
-            AuthUser::check_if_user_is_logged_in();
-        }
+
+        AuthUser::check_if_user_is_logged_in();
         $contents = require $this->getProjectDir() . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {

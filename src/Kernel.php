f1ece813a2aa0695ded62fd6d8620e26a8dc9977
Matican Matican <maticangroup@gmail.com>
2019-08-11 21:30:34 +0430

    Amirhossein

--- src/Kernel.php
+++ src/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends BaseKernel
         Settings::set('CLIENT_IP', '');
         Settings::set('CLIENT_ACCESS_TOKEN', '');
         Settings::set('APPLICATION_DOMAIN', 'http://127.0.0.1:8000');
-        Settings::set('SERVER_DOMAIN', 'http://core-dev.maticangroup.com');
+        Settings::set('SERVER_DOMAIN', 'http://core.maticangroup.com');
         AuthUser::check_if_user_is_logged_in();
         /*### MATICAN ###*/
 
